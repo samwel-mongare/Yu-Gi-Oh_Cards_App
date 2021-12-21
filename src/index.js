4ade361849f774c529d6ce20b290b039ebdd046d
od-c0d3r <omar_rashad@aol.com>
2021-12-21 17:13:29 +0200

    Fix: linters resolved

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles/style.css';
-import './styles/popup.css'
+import './styles/popup.css';
 import './modules/module1.js';
 import './modules/popup.js';
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,26 +1,26 @@
-let cardObj = {
-    "id": 12321,
-    "name": "Tornado Dragon",
-    "type": "XYZ Monster",
-    "desc": "2 Level 4 monsters\nOnce per turn (Quick Effect): You can detach 1 material from this card, then target 1 Spell/Trap on the field; destroy it.",
-    "atk": 2100,
-    "def": 2000,
-    "attribute": "WIND",
-    "card_images": [
-        {
-            "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/6983839.jpg",
-            "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/6983839.jpg"            
-        }
-    ],
-    "card_prices": [
-        {
-            "cardmarket_price": "0.42",
-            "tcgplayer_price": "0.48",
-            "ebay_price": "2.99",
-            "amazon_price": "0.77",
-            "coolstuffinc_price": "0.99"
-        }
-    ]
+const cardObj = {
+  id: 12321,
+  name: 'Tornado Dragon',
+  type: 'XYZ Monster',
+  desc: '2 Level 4 monsters\nOnce per turn (Quick Effect): You can detach 1 material from this card, then target 1 Spell/Trap on the field; destroy it.',
+  atk: 2100,
+  def: 2000,
+  attribute: 'WIND',
+  card_images: [
+    {
+      image_url: 'https://storage.googleapis.com/ygoprodeck.com/pics/6983839.jpg',
+      image_url_small: 'https://storage.googleapis.com/ygoprodeck.com/pics_small/6983839.jpg',
+    },
+  ],
+  card_prices: [
+    {
+      cardmarket_price: '0.42',
+      tcgplayer_price: '0.48',
+      ebay_price: '2.99',
+      amazon_price: '0.77',
+      coolstuffinc_price: '0.99',
+    },
+  ],
 };
 
 // will use this if the input is id <<<<<<<<<<<<<<<
@@ -31,45 +31,47 @@ let cardObj = {
 // }
 
 function displayPopup(idOrCardObj) {
-    // let idOrCardObj = getCard(id);  i will use this if the input is id <<<<<<<<<<<<<<<<<
-    let [popup, x, popupBody] = ["appPopup", 'close', 'popup-body'].map(id => document.getElementById(id));
-    popup.style.display = "block";
+  // let idOrCardObj = getCard(id);  i will use this if the input is id <<<<<<<<<<<<<<<<<
+  const [popup, xBtn, popupBody] = ['appPopup', 'close', 'popup-body'].map((id) => document.getElementById(id));
+  popup.style.display = 'block';
 
-    let cardImg = document.createElement('img');
-    let cardTitle = document.createElement('h2');
-    let cardDesc = document.createElement('div');
-    let cardType = document.createElement('span');
-    let cardATK = document.createElement('span');
-    let cardDEF = document.createElement('span');
-    let cardAttr = document.createElement('span');
+  const cardImg = document.createElement('img');
+  const cardTitle = document.createElement('h2');
+  const cardDesc = document.createElement('div');
+  const cardType = document.createElement('span');
+  const cardATK = document.createElement('span');
+  const cardDEF = document.createElement('span');
+  const cardAttr = document.createElement('span');
 
-    cardDesc.className = 'desc';
-    cardATK.className = 'itemLeft';
-    cardDEF.className = 'itemLeft';
+  cardDesc.className = 'desc';
+  cardATK.className = 'itemLeft';
+  cardDEF.className = 'itemLeft';
 
-    cardImg.src = idOrCardObj.card_images[0].image_url;
-    cardTitle.innerText = `${idOrCardObj.name}`
-    cardType.innerText = `Type : ${idOrCardObj.type}`;
-    cardATK.innerText = `Attack : ${idOrCardObj.atk}`;
-    cardAttr.innerText = `Attribute : ${idOrCardObj.attribute}`;
-    cardDEF.innerText = `Defence : ${idOrCardObj.def}`;
-    cardDesc.innerText = '';
-    cardDesc.append(cardType, cardATK, cardAttr, cardDEF)
+  cardImg.src = idOrCardObj.card_images[0].image_url;
+  cardTitle.innerText = `${idOrCardObj.name}`;
+  cardType.innerText = `Type : ${idOrCardObj.type}`;
+  cardATK.innerText = `Attack : ${idOrCardObj.atk}`;
+  cardAttr.innerText = `Attribute : ${idOrCardObj.attribute}`;
+  cardDEF.innerText = `Defence : ${idOrCardObj.def}`;
+  cardDesc.innerText = '';
+  cardDesc.append(cardType, cardATK, cardAttr, cardDEF);
 
-    popupBody.innerHTML = ''
-    popupBody.append(cardImg, cardTitle, cardDesc);
+  popupBody.innerHTML = '';
+  popupBody.append(cardImg, cardTitle, cardDesc);
 
-    x.onclick = function () {
-        popup.style.display = "none";
+  xBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+      popup.style.display = 'none';
     }
-    window.onclick = function (event) {
-        if (event.target == popup) {
-            popup.style.display = "none";
-        }
-    }
+  });
 }
 
-let btn = document.getElementById("myBtn");
+const btn = document.getElementById('myBtn');
 btn.addEventListener('click', (e) => {
-    displayPopup(cardObj);
-})
+  displayPopup(cardObj);
+  e.preventDefault();
+});
