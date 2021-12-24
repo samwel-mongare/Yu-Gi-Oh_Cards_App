8225f2ffc90a1a5632703e856920c9ad78433f5d
od-c0d3r <omar_rashad@aol.com>
2021-12-24 16:41:40 +0200

    Fix: Fix: Plus equal operator used

--- src/modules/counting.js
+++ src/modules/counting.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-plusplus */
-
 import { cards } from './module1.js';
 
 const counting = document.getElementById('supy');
@@ -7,7 +5,7 @@ const counting = document.getElementById('supy');
 function arrayLength(a) {
   let length = 0;
   while (a[length] !== undefined) {
-    length++;
+    length += 1;
   }
   counting.innerHTML = `(${length})`;
 }
